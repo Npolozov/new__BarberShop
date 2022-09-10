3706b45ad172710cb1fa2c2d1bb502f9a5d6187a
Nikita Polozov <npolozov0@gmail.com>
2022-09-10 20:20:48 +0300

    !

---
+++ js/mobal-menu.js
@@ -0,0 +1,17 @@
+(() => {
+  const menuBtnRef = document.querySelectorAll("[data-menu-button]");
+  const mainButtonRef = menuBtnRef[0];
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.forEach((el) =>
+    el.addEventListener("click", () => {
+      const expanded =
+        mainButtonRef.getAttribute("aria-expanded") === "true" || false;
+
+      mainButtonRef.classList.toggle("is-open");
+      mainButtonRef.setAttribute("aria-expanded", !expanded);
+
+      mobileMenuRef.classList.toggle("is-open");
+    })
+  );
+})();
